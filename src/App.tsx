b5917ea97b1f796c2fa3504e60a0ccfb80ee7dca
SrNanu <107449931+SrNanu@users.noreply.github.com>
2025-02-24 17:39:26 -0300

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,25 @@ function App() {
   const handleSelect2 = (element: string) => {
     console.log("Mostrando ", element);
   };
+  const contenido = 
+    list.length > 0 ? (
+      <List data={list} onselect={handleSelect} />
+    ) : (
+      "No hay elementos"
+    );
 
   return (
     <Card>
       <CardBody title="Hola mundo" text="Texto de la carta" />
-      <List data={list} onselect={handleSelect} />
-      <List data={list} onselect={handleSelect2} />
+      {contenido}
     </Card>
   );
 }
 
 export default App;
+
+//Diference between state and props
+// State is mutable and props is immutable
+// State is used to store data that can change over time
+// Props is used to pass data from parent to child
+// Both are used to render the UI
